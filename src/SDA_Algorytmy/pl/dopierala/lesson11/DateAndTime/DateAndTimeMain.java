531d3b0cffbf943a9eb2be1d52e8b0a4b67e1fc4
Piotr Dopierala <ptr.dopierala@gmail.com>
2018-10-27 10:56:38 +0200

    Date and Time exercises first commit.

---
+++ src/SDA_Algorytmy/pl/dopierala/lesson11/DateAndTime/DateAndTimeMain.java
@@ -0,0 +1,20 @@
+package SDA_Algorytmy.pl.dopierala.lesson11.DateAndTime;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateAndTimeMain {
+    public static void main(String[] args) {
+        LocalDateTime actualDate = LocalDateTime.now();
+        DateTimeFormatter formater = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        LocalDateTime accident = LocalDateTime.parse("21-05-2017 09:25",formater);
+
+        System.out.println("Dzisiaj mamy:"+actualDate.format(formater));
+        System.out.println("Wypadek miałem :"+ accident.format(formater));
+
+        System.out.println(Duration.between(accident,actualDate).toDays());
+
+    }
+}

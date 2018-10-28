16422b01c761c57f60b827c97e8ec0f59056e614
Piotr Dopierala <ptr.dopierala@gmail.com>
2018-10-28 14:20:22 +0100

    - changed sorting algorithms to allow generics - added QuickSort implementation

--- src/SDA_Algorytmy/pl/dopierala/lesson12/InsertSort/InsertSortMain.java
+++ src/SDA_Algorytmy/pl/dopierala/lesson12/InsertSort/InsertSortMain.java
@@ -2,23 +2,53 @@ package SDA_Algorytmy.pl.dopierala.lesson12.InsertSort;
 
 public class InsertSortMain {
     public static void main(String[] args) {
-        int[] intArray = {7,3,8,5,2};
-        insertSort(intArray);
+        Integer[] intArray = {7, 3, 8, 5, 2};
+        insertSort2(intArray);
+        printArray(intArray);
     }
 
-    public static int[] insertSort(int[] in){
+    public static <T extends Comparable> void insertSort(T[] in) {
         int n = in.length;
         int i;
 
-        for (int j = n-2; j >= 0; j--) { //j is index of value we want to insert in order
-            int x = in[j]; //take number to insert.
-            for (i = j+1; i < n; i++) { //i iterates over already sorted part of array
-                if(x<in[i]) //
+        for (int j = n - 2; j >= 0; j--) { //j is index of value we want to insert in order
+            T x = in[j]; //take number to insert.
+            for (i = j + 1; i < n; i++) { //i iterates over already sorted part of array
+                if (x.compareTo(in[i])<0)//(x < in[i])
                     break;
-                in[i-1]=in[i];//move sorted element to the left.
+                in[i - 1] = in[i];//move sorted element to the left.
             }
-            in[i-1]=x;
+            in[i - 1] = x;
         }
-        return in;
     }
+
+    public static <T extends Comparable> void insertSort2(T[] in) {
+        insertSortRecurse(in, in.length - 1);
+    }
+
+
+    private static <T extends Comparable> void insertSortRecurse(T[] in, int i) {
+        int j;
+        int n = in.length;
+        T x = in[i - 1];
+
+        for (j = i; j < n; j++) {
+            if (in[j].compareTo(x)<0){//(in[j] < x) {
+                in[j - 1] = in[j];
+            } else
+                break;
+        }
+        in[j - 1] = x;
+        if (i - 1 > 0)
+            insertSortRecurse(in, i - 1);
+    }
+
+    private static <T> void printArray(T[] in) {
+        System.out.printf("[");
+        for (int i = 0; i < in.length - 1; i++) {
+            System.out.printf("%s,", in[i]);
+        }
+        System.out.printf("%s]\n", in[in.length - 1]);
+    }
+
 }
---
+++ src/SDA_Algorytmy/pl/dopierala/lesson12/QuickSort/QuickSortMain.java
@@ -0,0 +1,54 @@
+package SDA_Algorytmy.pl.dopierala.lesson12.QuickSort;
+
+public class QuickSortMain {
+    public static void main(String[] args) {
+        Integer[] intArray = {9,8,7,6,1,2,3,5,3,2,4,7,5};
+        String[] stringArray = {"Zuzanna","Zzuanna","Dorian","Ala","Piotrek",};
+
+        //quickSort(intArray,0,intArray.length-1);
+        quickSort(stringArray,0,stringArray.length-1);
+        printArray(stringArray);
+
+        String arg;
+        arg=null;
+        "Tak".equals(arg);
+
+    }
+
+    public static <T extends Comparable> void quickSort(T[] in,int left, int right){
+
+        int pwtIdx=(left+right)/2;
+        T pwt=in[pwtIdx];
+        in[pwtIdx]=in[right];
+
+        int j=left;
+        int i=left;
+
+        for(i=left;i<right;i++){ //lewa strona
+            if(in[i].compareTo(pwt)<0){
+                T temp = in[i]; //replace
+                in[i]=in[j];
+                in[j]=temp;
+                j++;
+            }
+        }
+
+        in[right]=in[j];
+        in[j]=pwt;
+
+        if(left<(j-1))
+            quickSort(in,left,j-1);
+        if((j+1)<right){
+            quickSort(in,j+1,right);
+        }
+
+    }
+
+    private static <T> void printArray(T[] in) {
+        System.out.printf("[");
+        for (int i = 0; i < in.length - 1; i++) {
+            System.out.printf("%s,", in[i]);
+        }
+        System.out.printf("%s]\n", in[in.length - 1]);
+    }
+}
